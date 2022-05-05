0e09cb69cb19e9f924eb68eae0c056317daaf48e
CryptoSnowPrince <topdirector2017@gmail.com>
2022-05-05 20:20:09 +0200

    add SimpleTokenSwap and deploy and verify

--- SmartContract/hardhat.config.ts
+++ SmartContract/hardhat.config.ts
@@ -44,6 +44,15 @@ const config: HardhatUserConfig = {
         }
       },
       {
+        version: "0.7.3",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 2000
+          }
+        }
+      },
+      {
         version: "0.8.0",
         settings: {
           optimizer: {
