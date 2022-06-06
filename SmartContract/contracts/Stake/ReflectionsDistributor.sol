a8270d404b69554993612e4b78a03a121bbbe6c8
CryptoSnowPrince <topdirector2017@gmail.com>
2022-06-07 02:42:59 +0900

    update ReflectionDistributor

--- SmartContract/contracts/Stake/ReflectionsDistributor.sol
+++ SmartContract/contracts/Stake/ReflectionsDistributor.sol
@@ -1,16 +1,12 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.11;
-
+pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract ReflectionsDistributor is Ownable {
-    using SafeMath for uint256;
-
-    /// @notice Info of each user 
+    /// @notice Info of each user
     struct UserInfo {
         uint256 amount;
         uint256 rewardDebt;
@@ -34,7 +30,7 @@ contract ReflectionsDistributor is Ownable {
     /// this allows to reward users with EMPIRE
     uint256 public internalEmpireBalance;
 
-    /// @notice Last reward balance 
+    /// @notice Last reward balance
     uint256 public lastRewardBalance;
 
     /// @notice Accumulated rewards per share, scaled to `ACC_REWARD_PER_SHARE_PRECISION`
@@ -55,17 +51,10 @@ contract ReflectionsDistributor is Ownable {
     /// @notice Emitted when a user claims reward
     event ClaimReward(address indexed user, uint256 amount);
 
-    /// @notice Emitted when a user emergency withdraws its EMPIRE
-    event EmergencyWithdraw(address indexed user, uint256 amount);
-
-
-
     /**
      * @param _empire The address of the EMPIRE token
      */
     constructor(IERC20 _empire) {
-        require(address(_empire) != address(0), "EMPIREStaking: Empire can't be address(0)");
-        
         empire = _empire;
 
         ACC_REWARD_PER_SHARE_PRECISION = 1e24;
@@ -79,39 +68,27 @@ contract ReflectionsDistributor is Ownable {
         UserInfo storage user = userInfo[_user];
 
         uint256 _previousAmount = user.amount;
-        uint256 _newAmount = user.amount.add(_amount);
+        uint256 _newAmount = user.amount + _amount;
         user.amount = _newAmount;
 
         updateReward();
         uint256 _previousRewardDebt = user.rewardDebt;
-        user.rewardDebt = _newAmount.mul(accRewardPerShare).div(ACC_REWARD_PER_SHARE_PRECISION);
+        user.rewardDebt =
+            (_newAmount * accRewardPerShare) /
+            ACC_REWARD_PER_SHARE_PRECISION;
         if (_previousAmount != 0) {
-            uint256 _pending = _previousAmount
-                .mul(accRewardPerShare)
-                .div(ACC_REWARD_PER_SHARE_PRECISION)
-                .sub(_previousRewardDebt);
+            uint256 _pending = (_previousAmount * accRewardPerShare) /
+                ACC_REWARD_PER_SHARE_PRECISION -
+                _previousRewardDebt;
             if (_pending != 0) {
                 safeTokenTransfer(_user, _pending);
                 emit ClaimReward(_user, _pending);
             }
         }
-    
 
-        internalEmpireBalance = internalEmpireBalance.add(_amount);
+        internalEmpireBalance += _amount;
         emit Deposit(_user, _amount);
     }
-
-    /**
-     * @notice Get user info
-     * @param _user The address of the user
-     * @return The amount of EMPIRE user has deposited
-     * @return The reward debt for the chosen token
-     */
-    function getUserInfo(address _user) external view returns (uint256, uint256) {
-        UserInfo storage user = userInfo[_user];
-        return (user.amount, user.rewardDebt);
-    }
-
 
     /**
      * @notice View function to see pending reward token on frontend
@@ -119,7 +96,6 @@ contract ReflectionsDistributor is Ownable {
      * @return `_user`'s pending reward token
      */
     function pendingReward(address _user) external view returns (uint256) {
-
         UserInfo storage user = userInfo[_user];
         uint256 _totalEmpire = internalEmpireBalance;
         uint256 _accRewardTokenPerShare = accRewardPerShare;
@@ -128,13 +104,16 @@ contract ReflectionsDistributor is Ownable {
         uint256 _rewardBalance = _currRewardBalance;
 
         if (_rewardBalance != lastRewardBalance && _totalEmpire != 0) {
-            uint256 _accruedReward = _rewardBalance.sub(lastRewardBalance);
-            _accRewardTokenPerShare = _accRewardTokenPerShare.add(
-                _accruedReward.mul(ACC_REWARD_PER_SHARE_PRECISION).div(_totalEmpire)
-            );
+            uint256 _accruedReward = _rewardBalance - lastRewardBalance;
+            _accRewardTokenPerShare =
+                _accRewardTokenPerShare +
+                (_accruedReward * ACC_REWARD_PER_SHARE_PRECISION) /
+                _totalEmpire;
         }
         return
-            user.amount.mul(_accRewardTokenPerShare).div(ACC_REWARD_PER_SHARE_PRECISION).sub(user.rewardDebt);
+            (user.amount * _accRewardTokenPerShare) /
+            ACC_REWARD_PER_SHARE_PRECISION -
+            user.rewardDebt;
     }
 
     /**
@@ -144,26 +123,29 @@ contract ReflectionsDistributor is Ownable {
     function withdraw(address _user, uint256 _amount) external {
         UserInfo storage user = userInfo[_user];
         uint256 _previousAmount = user.amount;
-        require(_amount <= _previousAmount, "ReflectionsDistributor: withdraw amount exceeds balance");
-        uint256 _newAmount = user.amount.sub(_amount);
+        require(
+            _amount <= _previousAmount,
+            "ReflectionsDistributor: withdraw amount exceeds balance"
+        );
+        uint256 _newAmount = user.amount - (_amount);
         user.amount = _newAmount;
 
-    
-        if (_previousAmount != 0) { 
-            updateReward();   
-            uint256 _pending = _previousAmount
-                .mul(accRewardPerShare)
-                .div(ACC_REWARD_PER_SHARE_PRECISION)
-                .sub(user.rewardDebt);
-            user.rewardDebt = _newAmount.mul(accRewardPerShare).div(ACC_REWARD_PER_SHARE_PRECISION) ;  
+        if (_previousAmount != 0) {
+            updateReward();
+            uint256 _pending = (_previousAmount * accRewardPerShare) /
+                ACC_REWARD_PER_SHARE_PRECISION -
+                user.rewardDebt;
+            user.rewardDebt =
+                (_newAmount * accRewardPerShare) /
+                ACC_REWARD_PER_SHARE_PRECISION;
             if (_pending != 0) {
                 safeTokenTransfer(_user, _pending);
                 emit ClaimReward(_user, _pending);
             }
         }
 
-        internalEmpireBalance = internalEmpireBalance.sub(_amount);
-        
+        internalEmpireBalance -= _amount;
+
         emit Withdraw(_user, _amount);
     }
 
@@ -172,24 +154,22 @@ contract ReflectionsDistributor is Ownable {
      * @dev Needs to be called before any deposit or withdrawal
      */
     function updateReward() public {
-        
-
         uint256 _totalEmpire = internalEmpireBalance;
 
         uint256 _currRewardBalance = empire.balanceOf(address(this));
         uint256 _rewardBalance = _currRewardBalance;
-    
 
         // Did ReflectionsDistributor receive any token
         if (_rewardBalance == lastRewardBalance || _totalEmpire == 0) {
             return;
         }
 
-        uint256 _accruedReward = _rewardBalance.sub(lastRewardBalance);
+        uint256 _accruedReward = _rewardBalance - lastRewardBalance;
 
-        accRewardPerShare = accRewardPerShare.add(
-            _accruedReward.mul(ACC_REWARD_PER_SHARE_PRECISION).div(_totalEmpire)
-        );
+        accRewardPerShare =
+            accRewardPerShare +
+            (_accruedReward * ACC_REWARD_PER_SHARE_PRECISION) /
+            _totalEmpire;
         lastRewardBalance = _rewardBalance;
     }
 
@@ -199,19 +179,31 @@ contract ReflectionsDistributor is Ownable {
      * @param _to The address that will receive `_amount` `rewardToken`
      * @param _amount The amount to send to `_to`
      */
-    function safeTokenTransfer(
-        address _to,
-        uint256 _amount
-    ) internal {
+    function safeTokenTransfer(address _to, uint256 _amount) internal {
         uint256 _currRewardBalance = empire.balanceOf(address(this));
         uint256 _rewardBalance = _currRewardBalance;
 
         if (_amount > _rewardBalance) {
-            lastRewardBalance = lastRewardBalance.sub(_rewardBalance);
+            lastRewardBalance = lastRewardBalance - _rewardBalance;
             empire.transfer(_to, _rewardBalance);
         } else {
-            lastRewardBalance = lastRewardBalance.sub(_amount);
+            lastRewardBalance = lastRewardBalance - _amount;
             empire.transfer(_to, _amount);
         }
+    }
+
+    /**
+     * @notice Get user info
+     * @param _user The address of the user
+     * @return The amount of EMPIRE user has deposited
+     * @return The reward debt for the chosen token
+     */
+    function getUserInfo(address _user)
+        external
+        view
+        returns (uint256, uint256)
+    {
+        UserInfo storage user = userInfo[_user];
+        return (user.amount, user.rewardDebt);
     }
 }
