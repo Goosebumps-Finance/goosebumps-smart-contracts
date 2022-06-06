314f58b796a79e2587426ab0307768dbfbeec34b
CryptoSnowPrince <topdirector2017@gmail.com>
2022-06-06 15:29:21 +0900

    update deployStaking script add Staking.js to verify with hardhat command

---
+++ SmartContract/verify/Staking.js
@@ -0,0 +1,15 @@
+const _stakeToken = "0xD6cf8aeEeBf84C43e28934D6D743820195BBa59C";
+const _rewardsToken = "0xA1a83bC7712f65E646933a5E00A90537Fc847835";
+const _treasury = "0x69000D6Da4eedf452a1e2048BEeEc901BcdCE82E";
+const _rewardWallet = "0x69000D6Da4eedf452a1e2048BEeEc901BcdCE82E";
+const _rewardPerBlockTokenN = 100;
+const _rewardPerBlockTokenD = 100;
+
+module.exports = [
+    _stakeToken,
+    _rewardsToken,
+    _treasury,
+    _rewardWallet,
+    _rewardPerBlockTokenN,
+    _rewardPerBlockTokenD,
+];
