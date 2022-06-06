b0249aafc8ae1455e8fccdb5a55866c2e3c8e589
CryptoSnowPrince <topdirector2017@gmail.com>
2022-06-06 15:32:38 +0900

    update deploy scripts with contructor inParams

--- SmartContract/scripts/StakeAndFarm/deployFarming.ts
+++ SmartContract/scripts/StakeAndFarm/deployFarming.ts
@@ -15,10 +15,10 @@ async function main() {
 
   // We get the contract to deploy
 
-  const _lpToken = "0x256C9FbE9093E7b9E3C4584aDBC3066D8c6216da";
-  const _rewardsToken = "0x256C9FbE9093E7b9E3C4584aDBC3066D8c6216da";
-  const _treasury = "0x256C9FbE9093E7b9E3C4584aDBC3066D8c6216da";
-  const _rewardWallet = "0x256C9FbE9093E7b9E3C4584aDBC3066D8c6216da";
+  const _lpToken = "0xD6cf8aeEeBf84C43e28934D6D743820195BBa59C";
+  const _rewardsToken = "0xA1a83bC7712f65E646933a5E00A90537Fc847835";
+  const _treasury = "0x69000D6Da4eedf452a1e2048BEeEc901BcdCE82E";
+  const _rewardWallet = "0x69000D6Da4eedf452a1e2048BEeEc901BcdCE82E";
   const _rewardPerBlockTokenN = 100;
   const _rewardPerBlockTokenD = 100;
 
