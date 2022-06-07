50760849d8888c9ad5c1bed299e1cb6b6b0cf9e7
CryptoSnowPrince <topdirector2017@gmail.com>
2022-06-08 06:28:38 +0900

    add MiniRouter and Bridge

---
+++ SmartContract/contracts/Bridge/Bridge.sol
@@ -0,0 +1,262 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+interface IBridgeToken {
+    function transferByBridge(
+        address from,
+        address to,
+        uint256 tAmount
+    ) external;
+
+    function decimals() external pure returns (uint8);
+}
+
+interface IERC20 {
+    function balanceOf(address account) external view returns (uint256);
+
+    function transfer(address to, uint256 amount) external returns (bool);
+}
+
+contract Bridge is Ownable, Pausable, ReentrancyGuard {
+    // state variables
+
+    address public validator;
+    uint256 public fee = 1 * 10**(18 - 2); // 0.01 Ether
+    address payable public TREASURY;
+    address public POOL;
+
+    uint256 public minAmount = 1;
+    uint256 public maxAmount = 10000;
+
+    uint256 private currentNonce = 0;
+
+    mapping(address => mapping(uint256 => address)) public bridgeTokenPair;
+    mapping(bytes32 => bool) public processedRedeem;
+
+    // events list
+
+    event LogSetFee(uint256 fee);
+    event LogSetValidator(address validator);
+    event LogSetTreasury(address indexed treasury);
+    event LogSetPool(address indexed pool);
+    event LogSetMinAmount(uint256 minAmount);
+    event LogSetMaxAmount(uint256 maxAmount);
+    event LogUpdateBridgeTokenPairList(
+        address fromToken,
+        uint256 toChainId,
+        address toToken
+    );
+    event LogFallback(address from, uint256 amount);
+    event LogReceive(address from, uint256 amount);
+    event LogWithdrawalETH(address indexed recipient, uint256 amount);
+    event LogWithdrawalERC20(
+        address indexed token,
+        address indexed recipient,
+        uint256 amount
+    );
+    event LogSwap(
+        uint256 indexed nonce,
+        address indexed from,
+        uint256 fromChainId,
+        address fromToken,
+        address to,
+        uint256 toChainId,
+        address toToken,
+        uint256 amount
+    );
+    event LogRedeem(
+        bytes32 txs,
+        address token,
+        uint256 amount,
+        address to,
+        uint256 fromChainId
+    );
+
+    constructor(
+        address _validator,
+        address payable _treasury,
+        address _pool
+    ) {
+        validator = _validator;
+        TREASURY = _treasury;
+        POOL = _pool;
+    }
+
+    function swap(
+        address token,
+        uint256 amount,
+        address to,
+        uint256 toChainId
+    ) external payable whenNotPaused nonReentrant nonContract {
+        require(toChainId != cID(), "Invalid Bridge");
+        require(
+            bridgeTokenPair[token][toChainId] != address(0),
+            "Invalid Bridge Token"
+        );
+        require(
+            amount >= minAmount * (10**IBridgeToken(token).decimals()) &&
+                amount <= maxAmount * (10**IBridgeToken(token).decimals()),
+            "Wrong amount"
+        );
+        require(msg.value >= fee, "Fee is not fulfilled");
+
+        uint256 nonce = currentNonce;
+        currentNonce++;
+
+        // send fee to TREASURY address
+        TREASURY.transfer(msg.value);
+        IBridgeToken(token).transferByBridge(msg.sender, POOL, amount);
+
+        emit LogSwap(
+            nonce,
+            msg.sender,
+            cID(),
+            token,
+            to,
+            toChainId,
+            bridgeTokenPair[token][toChainId],
+            amount
+        );
+    }
+
+    function redeem(
+        bytes32 txs,
+        address token,
+        uint256 amount,
+        address to,
+        uint256 fromChainId
+    ) external onlyValidator whenNotPaused nonReentrant {
+        require(
+            amount >= minAmount * (10**IBridgeToken(token).decimals()) &&
+                amount <= maxAmount * (10**IBridgeToken(token).decimals()),
+            "Wrong amount"
+        );
+        require(fromChainId != cID(), "Invalid Bridge");
+
+        bytes32 hash_ = keccak256(abi.encodePacked(txs, fromChainId));
+        require(processedRedeem[hash_] != true, "Redeem already processed");
+        processedRedeem[hash_] = true;
+
+        IBridgeToken(token).transferByBridge(POOL, to, amount);
+
+        emit LogRedeem(txs, token, amount, to, fromChainId);
+    }
+
+    function isValidator() internal view returns (bool) {
+        return (validator == msg.sender);
+    }
+
+    modifier onlyValidator() {
+        require(isValidator(), "DENIED : Not Validator");
+        _;
+    }
+
+    function isContract(address account) internal view returns (bool) {
+        return account.code.length > 0;
+    }
+
+    modifier nonContract() {
+        require(!isContract(msg.sender), "contract not allowed");
+        require(msg.sender == tx.origin, "proxy contract not allowed");
+        _;
+    }
+
+    function cID() public view returns (uint256) {
+        uint256 id;
+        assembly {
+            id := chainid()
+        }
+        return id;
+    }
+
+    // Set functions
+
+    function setMinAmount(uint256 _minAmount) external onlyOwner {
+        require(_minAmount <= maxAmount, "MinAmount <= MaxAmount");
+        minAmount = _minAmount;
+
+        emit LogSetMinAmount(minAmount);
+    }
+
+    function setMaxAmount(uint256 _maxAmount) external onlyOwner {
+        require(_maxAmount >= minAmount, "MaxAmount >= MinAmount");
+        maxAmount = _maxAmount;
+
+        emit LogSetMaxAmount(maxAmount);
+    }
+
+    function setPool(address _pool) external onlyOwner {
+        POOL = _pool;
+        emit LogSetPool(POOL);
+    }
+
+    function updateBridgeTokenPairList(
+        address fromToken,
+        uint256 toChainId,
+        address toToken
+    ) external onlyOwner {
+        bridgeTokenPair[fromToken][toChainId] = toToken;
+        emit LogUpdateBridgeTokenPairList(fromToken, toChainId, toToken);
+    }
+
+    function setPause() external onlyOwner {
+        _pause();
+    }
+
+    function setUnpause() external onlyOwner {
+        _unpause();
+    }
+
+    function setValidator(address _validator) external onlyOwner {
+        validator = _validator;
+        emit LogSetValidator(validator);
+    }
+
+    function setTreasury(address payable _treasury) external onlyOwner {
+        TREASURY = _treasury;
+        emit LogSetTreasury(TREASURY);
+    }
+
+    function setFee(uint256 _fee) external onlyOwner {
+        fee = _fee;
+        emit LogSetFee(fee);
+    }
+
+    // Withdraw functions
+    function withdrawETH(address payable recipient, uint256 amount)
+        external
+        onlyOwner
+    {
+        require(amount <= (address(this)).balance, "INSUFFICIENT_FUNDS");
+        recipient.transfer(amount);
+        
+        emit LogWithdrawalETH(recipient, amount);
+    }
+
+    /**
+     * @notice Should not be withdrawn scam token.
+     */
+    function withdrawERC20(
+        IERC20 token,
+        address recipient,
+        uint256 amount
+    ) external onlyOwner {
+        require(amount <= token.balanceOf(address(this)), "INSUFFICIENT_FUNDS");
+        require(token.transfer(recipient, amount), "Transfer Fail");
+
+        emit LogWithdrawalERC20(address(token), recipient, amount);
+    }
+
+    // Receive and Fallback functions
+    receive() external payable {
+        emit LogReceive(msg.sender, msg.value);
+    }
+
+    fallback() external payable {
+        emit LogFallback(msg.sender, msg.value);
+    }
+}
---
+++ SmartContract/contracts/MiniRouter.sol
@@ -0,0 +1,506 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+interface IERC20 {
+    function balanceOf(address account) external view returns (uint256);
+
+    function transfer(address to, uint256 amount) external returns (bool);
+
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) external returns (bool);
+}
+
+interface IUniswapV2Factory {
+    function getPair(address tokenA, address tokenB)
+        external
+        view
+        returns (address pair);
+}
+
+interface IUniswapV2Router02 {
+    function factory() external pure returns (address);
+
+    function WETH() external pure returns (address);
+
+    function addLiquidity(
+        address tokenA,
+        address tokenB,
+        uint256 amountADesired,
+        uint256 amountBDesired,
+        uint256 amountAMin,
+        uint256 amountBMin,
+        address to,
+        uint256 deadline
+    )
+        external
+        returns (
+            uint256 amountA,
+            uint256 amountB,
+            uint256 liquidity
+        );
+
+    function addLiquidityETH(
+        address token,
+        uint256 amountTokenDesired,
+        uint256 amountTokenMin,
+        uint256 amountETHMin,
+        address to,
+        uint256 deadline
+    )
+        external
+        payable
+        returns (
+            uint256 amountToken,
+            uint256 amountETH,
+            uint256 liquidity
+        );
+
+    function removeLiquidity(
+        address tokenA,
+        address tokenB,
+        uint256 liquidity,
+        uint256 amountAMin,
+        uint256 amountBMin,
+        address to,
+        uint256 deadline
+    ) external returns (uint256 amountA, uint256 amountB);
+
+    function removeLiquidityETH(
+        address token,
+        uint256 liquidity,
+        uint256 amountTokenMin,
+        uint256 amountETHMin,
+        address to,
+        uint256 deadline
+    ) external returns (uint256 amountToken, uint256 amountETH);
+}
+
+interface IEmpire {
+    function isExcludedFromFee(address account) external view returns (bool);
+}
+
+contract MiniRouter is Ownable, Pausable, ReentrancyGuard {
+    ///@notice MiniRouter must be excluded from Empire buy/sell fee
+    address public empire;
+
+    ///@notice The only owner can add or remove new router, but before add, owner must check its contract.
+    mapping(address => bool) public supportedRouters;
+
+    ///@notice The only owner can add or remove new token, but before add, owner must check token contract.
+    mapping(address => bool) public supportedTokens;
+
+    event LogUpdateSupportedRouters(address router, bool enabled);
+    event LogUpdateSupportedTokens(address token, bool enabled);
+    event LogSetEmpire(address empire);
+    event LogFallback(address from, uint256 amount);
+    event LogReceive(address from, uint256 amount);
+    event LogWithdrawalETH(address indexed recipient, uint256 amount);
+    event LogWithdrawToken(
+        address indexed token,
+        address indexed recipient,
+        uint256 amount
+    );
+    event LogAddLiquidityTokens(
+        address indexed from,
+        address indexed router,
+        address indexed tokenB,
+        uint256 amountEmpire,
+        uint256 amountTokenB,
+        uint256 liquidity,
+        address to
+    );
+    event LogAddLiquidityETH(
+        address indexed from,
+        address indexed router,
+        uint256 amountEmpire,
+        uint256 amountETH,
+        uint256 liquidity,
+        address to
+    );
+    event LogRemoveLiquidityTokens(
+        address indexed from,
+        address indexed router,
+        address indexed tokenB,
+        uint256 liquidity,
+        uint256 amountEmpire,
+        uint256 amountTokenB,
+        address to
+    );
+    event LogRemoveLiquidityETH(
+        address indexed from,
+        address indexed router,
+        uint256 liquidity,
+        uint256 amountEmpire,
+        uint256 amountETH,
+        address to
+    );
+
+    constructor(
+        address empire_,
+        address router_,
+        address weth_,
+        address busd_
+    ) {
+        setEmpire(empire_);
+
+        updateSupportedRouters(router_, true);
+
+        updateSupportedTokens(weth_, true);
+        updateSupportedTokens(busd_, true);
+    }
+
+    function ensure(address router) private view {
+        require(
+            IEmpire(empire).isExcludedFromFee(address(this)) == true,
+            "MiniRouter: The Router must be excluded from fee"
+        );
+
+        require(
+            supportedRouters[router] == true,
+            "MiniRouter: The Router is not supported"
+        );
+    }
+
+    modifier ensureAddLiquidity(address router, uint256 amountEmpireDesired) {
+        ensure(router);
+
+        require(
+            IERC20(empire).transferFrom(
+                msg.sender,
+                address(this),
+                amountEmpireDesired
+            ),
+            "MiniRouter: TransferFrom failed"
+        );
+
+        require(
+            IERC20(empire).approve(router, amountEmpireDesired),
+            "MiniRouter: Approve failed"
+        );
+
+        _;
+    }
+
+    modifier ensureRemoveLiquidity(
+        address router,
+        address tokenB,
+        uint256 liquidity
+    ) {
+        ensure(router);
+
+        require(
+            supportedTokens[tokenB] == true,
+            "MiniRouter: The TokenB is not supported"
+        );
+
+        address pair = IUniswapV2Factory(IUniswapV2Router02(router).factory())
+            .getPair(empire, tokenB);
+
+        require(pair != address(0), "MiniRouter: Pair does not exist");
+
+        require(
+            IERC20(pair).transferFrom(msg.sender, address(this), liquidity),
+            "MiniRouter: TransferFrom failed"
+        );
+
+        require(
+            IERC20(pair).approve(router, liquidity),
+            "MiniRouter: Approve failed"
+        );
+
+        _;
+    }
+
+    function beforeAddLiquidityTokens(
+        address router,
+        address tokenB,
+        uint256 amountTokenBDesired
+    ) private {
+        require(
+            supportedTokens[tokenB] == true,
+            "MiniRouter: The TokenB is not supported"
+        );
+
+        require(
+            IERC20(tokenB).transferFrom(
+                msg.sender,
+                address(this),
+                amountTokenBDesired
+            ),
+            "MiniRouter: TransferFrom failed"
+        );
+
+        require(
+            IERC20(tokenB).approve(router, amountTokenBDesired),
+            "MiniRouter: Approve failed"
+        );
+    }
+
+    function addLiquidityTokens(
+        address router,
+        address tokenB,
+        uint256 amountEmpireDesired,
+        uint256 amountTokenBDesired,
+        address to,
+        uint256 deadline
+    )
+        external
+        whenNotPaused
+        nonReentrant
+        ensureAddLiquidity(router, amountEmpireDesired)
+        returns (
+            uint256 amountA,
+            uint256 amountB,
+            uint256 liquidity
+        )
+    {
+        beforeAddLiquidityTokens(router, tokenB, amountTokenBDesired);
+
+        (amountA, amountB, liquidity) = IUniswapV2Router02(router).addLiquidity(
+            empire,
+            tokenB,
+            amountEmpireDesired,
+            amountTokenBDesired,
+            0,
+            0,
+            to,
+            deadline
+        );
+
+        uint256 amountEmpireRefund = amountEmpireDesired - amountA;
+        uint256 amountTokenBRefund = amountTokenBDesired - amountB;
+
+        if (amountEmpireRefund > 0) {
+            require(
+                IERC20(empire).transfer(msg.sender, amountEmpireRefund),
+                "Transfer fail"
+            );
+        }
+
+        if (amountTokenBRefund > 0) {
+            require(
+                IERC20(tokenB).transfer(msg.sender, amountTokenBRefund),
+                "Transfer fail"
+            );
+        }
+
+        emit LogAddLiquidityTokens(
+            msg.sender,
+            router,
+            tokenB,
+            amountA,
+            amountB,
+            liquidity,
+            to
+        );
+    }
+
+    function addLiquidityETH(
+        address router,
+        uint256 amountEmpireDesired,
+        address to,
+        uint256 deadline
+    )
+        external
+        payable
+        whenNotPaused
+        nonReentrant
+        ensureAddLiquidity(router, amountEmpireDesired)
+        returns (
+            uint256 amountToken,
+            uint256 amountETH,
+            uint256 liquidity
+        )
+    {
+        (amountToken, amountETH, liquidity) = IUniswapV2Router02(router)
+            .addLiquidityETH{value: msg.value}(
+            empire,
+            amountEmpireDesired,
+            0,
+            0,
+            to,
+            deadline
+        );
+
+        uint256 amountEmpireRefund = amountEmpireDesired - amountToken;
+        uint256 amountETHRefund = msg.value - amountETH;
+
+        if (amountEmpireRefund > 0) {
+            require(
+                IERC20(empire).transfer(msg.sender, amountEmpireRefund),
+                "Transfer fail"
+            );
+        }
+
+        if (amountETHRefund > 0) {
+            (bool success, ) = msg.sender.call{value: amountETHRefund}(
+                new bytes(0)
+            );
+            require(success, "ETH Transfer fail");
+        }
+
+        emit LogAddLiquidityETH(
+            msg.sender,
+            router,
+            amountToken,
+            amountETH,
+            liquidity,
+            to
+        );
+    }
+
+    function removeLiquidityTokens(
+        address router,
+        address tokenB,
+        uint256 liquidity,
+        address to,
+        uint256 deadline
+    )
+        external
+        whenNotPaused
+        nonReentrant
+        ensureRemoveLiquidity(router, tokenB, liquidity)
+        returns (uint256 amountA, uint256 amountB)
+    {
+        (amountA, amountB) = IUniswapV2Router02(router).removeLiquidity(
+            empire,
+            tokenB,
+            liquidity,
+            0,
+            0,
+            address(this),
+            deadline
+        );
+
+        if (amountA > 0) {
+            require(IERC20(empire).transfer(to, amountA), "Transfer fail");
+        }
+
+        if (amountB > 0) {
+            require(IERC20(tokenB).transfer(to, amountB), "Transfer fail");
+        }
+
+        emit LogRemoveLiquidityTokens(
+            msg.sender,
+            router,
+            tokenB,
+            liquidity,
+            amountA,
+            amountB,
+            to
+        );
+    }
+
+    function removeLiquidityETH(
+        address router,
+        uint256 liquidity,
+        address to,
+        uint256 deadline
+    )
+        external
+        whenNotPaused
+        nonReentrant
+        ensureRemoveLiquidity(
+            router,
+            IUniswapV2Router02(router).WETH(),
+            liquidity
+        )
+        returns (uint256 amountToken, uint256 amountETH)
+    {
+        require(
+            IEmpire(empire).isExcludedFromFee(router) == true,
+            "MiniRouter: The `router` must be excluded from fee"
+        );
+
+        (amountToken, amountETH) = IUniswapV2Router02(router)
+            .removeLiquidityETH(
+                empire,
+                liquidity,
+                0,
+                0,
+                to,
+                deadline
+            );
+
+        emit LogRemoveLiquidityETH(
+            msg.sender,
+            router,
+            liquidity,
+            amountToken,
+            amountETH,
+            to
+        );
+    }
+
+    receive() external payable {
+        emit LogReceive(msg.sender, msg.value);
+    }
+
+    fallback() external payable {
+        emit LogFallback(msg.sender, msg.value);
+    }
+
+    function setPause() external onlyOwner {
+        _pause();
+    }
+
+    function setUnpause() external onlyOwner {
+        _unpause();
+    }
+
+    function setEmpire(address empire_) public onlyOwner {
+        empire = empire_;
+        emit LogSetEmpire(empire_);
+    }
+
+    function updateSupportedRouters(address router, bool enabled)
+        public
+        onlyOwner
+    {
+        supportedRouters[router] = enabled;
+
+        emit LogUpdateSupportedRouters(router, enabled);
+    }
+
+    function updateSupportedTokens(address token, bool enabled)
+        public
+        onlyOwner
+    {
+        supportedTokens[token] = enabled;
+
+        emit LogUpdateSupportedTokens(token, enabled);
+    }
+
+    function withdrawETH(address payable recipient, uint256 amount)
+        external
+        onlyOwner
+    {
+        require(amount <= (address(this)).balance, "INSUFFICIENT_FUNDS");
+        recipient.transfer(amount);
+        emit LogWithdrawalETH(recipient, amount);
+    }
+
+    /**
+     * @notice  Should not be withdrawn scam token.
+     */
+    function withdrawToken(
+        IERC20 token,
+        address recipient,
+        uint256 amount
+    ) external onlyOwner {
+        require(amount <= token.balanceOf(address(this)), "INSUFFICIENT_FUNDS");
+        require(token.transfer(recipient, amount), "Transfer Fail");
+
+        emit LogWithdrawToken(address(token), recipient, amount);
+    }
+}
