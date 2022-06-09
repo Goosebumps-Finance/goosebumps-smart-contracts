4c8cc8bdf17fd370bd63bf2bf40df30f4dcdc32c
CryptoSnowPrince <topdirector2017@gmail.com>
2022-06-09 21:59:29 +0900

    add solc 0.8.6

--- SmartContract/hardhat.config.ts
+++ SmartContract/hardhat.config.ts
@@ -62,6 +62,15 @@ const config: HardhatUserConfig = {
         }
       },
       {
+        version: "0.8.6",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      },
+      {
         version: "0.8.7",
         settings: {
           optimizer: {
