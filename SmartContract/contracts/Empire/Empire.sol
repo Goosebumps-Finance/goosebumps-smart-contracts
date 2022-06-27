e8d808dff5961cc6c40f280628979c97eff02d69
CryptoSnowPrince <topdirector2017@gmail.com>
2022-06-28 02:15:23 +0900

    full check and update empire token contract add comments fix names and custome ownable contract add zero-address check after gas-check lock compiler version

--- SmartContract/contracts/Empire/Empire.sol
+++ SmartContract/contracts/Empire/Empire.sol
@@ -1,9 +1,187 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.7;
+pragma solidity 0.8.7;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+/**
+ * @dev Interface of the ERC20 standard as defined in the EIP.
+ */
+interface IERC20 {
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `to`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address to, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender)
+        external
+        view
+        returns (uint256);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `from` to `to` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) external returns (bool);
+}
+
+/**
+ * @dev Provides information about the current execution context, including the
+ * sender of the transaction and its data. While these are generally available
+ * via msg.sender and msg.data, they should not be accessed in such a direct
+ * manner, since when dealing with meta-transactions the account sending and
+ * paying for execution may not be the actual sender (as far as an application
+ * is concerned).
+ *
+ * This contract is only required for intermediate, library-like contracts.
+ */
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        return msg.data;
+    }
+}
+
+/**
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * By default, the owner account will be the one that deploys the contract. This
+ * can later be changed with {transferOwnership}.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * the owner.
+ */
+abstract contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(
+        address indexed previousOwner,
+        address indexed newOwner
+    );
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor() {
+        _transferOwnership(_msgSender());
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() external virtual onlyOwner {
+        _transferOwnership(address(0));
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) external virtual onlyOwner {
+        require(
+            newOwner != address(0),
+            "Ownable: new owner is the zero address"
+        );
+        _transferOwnership(newOwner);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Internal function without access restriction.
+     */
+    function _transferOwnership(address newOwner) internal virtual {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+}
 
 interface IUniswapV2Factory {
     function createPair(address tokenA, address tokenB)
@@ -43,6 +221,11 @@ interface IUniswapV2Router02 is IUniswapV2Router01 {
     ) external;
 }
 
+/**
+ * @dev Empire token contract
+ *
+ * The `owner` account of Empire token contract will be multi-sig wallet.
+ */
 contract Empire is IERC20, Ownable {
     mapping(address => uint256) private _rOwned;
     mapping(address => uint256) private _tOwned;
@@ -113,7 +296,7 @@ contract Empire is IERC20, Ownable {
     event LogSwapAndLiquify(
         uint256 tokensSwapped,
         uint256 ethReceived,
-        uint256 tokensIntoLiqudity
+        uint256 tokensIntoLiquidity
     );
     event LogSwapAndDistribute(
         uint256 forMarketing,
@@ -167,6 +350,9 @@ contract Empire is IERC20, Ownable {
         inSwapAndLiquify = false;
     }
 
+    /**
+     * @dev It is not need to do `Zero-address check` of input params because deployer will check this before deploy.
+     */
     constructor(
         address _router,
         address _marketingWallet,
@@ -319,6 +505,9 @@ contract Empire is IERC20, Ownable {
         return true;
     }
 
+    /**
+     * @dev Reflection by action of any holder of Empire token.
+     */
     function deliver(uint256 tAmount) external {
         address sender = _msgSender();
         require(
@@ -712,6 +901,7 @@ contract Empire is IERC20, Ownable {
     function addLiquidity(uint256 tokenAmount, uint256 ethAmount) private {
         _approve(address(this), address(uniswapV2Router), tokenAmount);
 
+        // Skip `Return value check`
         uniswapV2Router.addLiquidityETH{value: ethAmount}(
             address(this),
             tokenAmount,
@@ -880,34 +1070,46 @@ contract Empire is IERC20, Ownable {
         external
         onlyOwner
     {
+        require(account != address(0), "Zero Address");
+        require(
+            _isExcludedFromFee[account] != enabled,
+            "Already set the same value"
+        );
         _isExcludedFromFee[account] = enabled;
         emit LogSetExcludeFromFee(msg.sender, account, enabled);
     }
 
     function setMarketingWallet(address newWallet) external onlyOwner {
+        require(newWallet != address(0), "Zero Address");
+        require(newWallet != marketingWallet, "Same Address");
         marketingWallet = newWallet;
         emit LogSetMarketingWallet(msg.sender, marketingWallet);
     }
 
     function setBurnWallet(address newWallet) external onlyOwner {
+        require(newWallet != address(0), "Zero Address");
+        require(newWallet != burnWallet, "Same Address");
         burnWallet = newWallet;
         emit LogSetBurnWallet(msg.sender, burnWallet);
     }
 
     function setTeamWallet(address newWallet) external onlyOwner {
+        require(newWallet != address(0), "Zero Address");
+        require(newWallet != teamWallet, "Same Address");
         teamWallet = newWallet;
         emit LogSetTeamWallet(msg.sender, teamWallet);
     }
 
     function setLiquidityWallet(address newLiquidityWallet) external onlyOwner {
+        require(newLiquidityWallet != address(0), "Zero Address");
+        require(newLiquidityWallet != liquidityWallet, "Same Address");
         liquidityWallet = newLiquidityWallet;
-
         emit LogSetLiquidityWallet(msg.sender, newLiquidityWallet);
     }
 
     function setEnableTrading(bool enable) external onlyOwner {
+        require(isTradingEnabled != enable, "Already set the same value");
         isTradingEnabled = enable;
-
         emit LogSetEnableTrading(isTradingEnabled);
     }
 
@@ -918,6 +1120,14 @@ contract Empire is IERC20, Ownable {
         uint256 _tax,
         uint256 _team
     ) external onlyOwner {
+        require(
+            !(buyFee.autoLp == _lp &&
+                buyFee.marketing == _marketing &&
+                buyFee.burn == _burn &&
+                buyFee.tax == _tax &&
+                buyFee.team == _team),
+            "Nothing is changed"
+        );
         buyFee.autoLp = _lp;
         buyFee.marketing = _marketing;
         buyFee.burn = _burn;
@@ -934,6 +1144,14 @@ contract Empire is IERC20, Ownable {
         uint256 _tax,
         uint256 _team
     ) external onlyOwner {
+        require(
+            !(sellFee.autoLp == _lp &&
+                sellFee.marketing == _marketing &&
+                sellFee.burn == _burn &&
+                sellFee.tax == _tax &&
+                sellFee.team == _team),
+            "Nothing is changed"
+        );
         sellFee.autoLp = _lp;
         sellFee.marketing = _marketing;
         sellFee.burn = _burn;
@@ -943,28 +1161,49 @@ contract Empire is IERC20, Ownable {
         emit LogSetSellFees(msg.sender, sellFee);
     }
 
+    /**
+     * @notice  Owner must check the `pair` address before call `setAutomatedMarketMakerPair`.
+     */
     function setAutomatedMarketMakerPair(address pair, bool enabled)
         public
         onlyOwner
     {
+        require(pair != address(0), "Zero Address");
+        require(
+            automatedMarketMakerPairs[pair] != enabled,
+            "Already set the same value"
+        );
         automatedMarketMakerPairs[pair] = enabled;
 
         emit LogSetAutomatedMarketMakerPair(msg.sender, pair, enabled);
     }
 
+    /**
+     * @notice  Owner must check the `newRouter` address before call `setAutomatedMarketMakerPair`.
+     */
     function setRouterAddress(address newRouter) external onlyOwner {
+        require(newRouter != address(0), "Zero Address");
+        require(newRouter != address(uniswapV2Router), "Same Address");
         uniswapV2Router = IUniswapV2Router02(newRouter);
 
         emit LogSetRouterAddress(msg.sender, newRouter);
     }
 
     function setSwapAndLiquifyEnabled(bool _enabled) external onlyOwner {
+        require(
+            swapAndLiquifyEnabled != _enabled,
+            "Already set the same value"
+        );
         swapAndLiquifyEnabled = _enabled;
 
         emit LogSwapAndLiquifyEnabledUpdated(msg.sender, _enabled);
     }
 
     function setSwapTokensAmount(uint256 amount) external onlyOwner {
+        require(
+            numTokensSellToAddToLiquidity != amount,
+            "Already set the same value"
+        );
         numTokensSellToAddToLiquidity = amount;
 
         emit LogSetSwapTokensAmount(msg.sender, amount);
@@ -996,6 +1235,9 @@ contract Empire is IERC20, Ownable {
         emit LogIncludeInReward(account);
     }
 
+    /**
+     * @notice  Owner will withdraw ETH and will use to benefit the Empire token holders.
+     */
     function withdrawETH(address payable recipient, uint256 amount)
         external
         onlyOwner
@@ -1006,7 +1248,8 @@ contract Empire is IERC20, Ownable {
     }
 
     /**
-     * @notice  Should not be withdrawn scam token or this Empire token.
+     * @notice  Owner will withdraw ERC20 token that have the price and then will use to benefit the Empire token holders.
+     #          Should not be withdrawn scam token or this Empire token.
      *          Use `withdraw` function to withdraw this Empire token.
      */
     function withdrawToken(
@@ -1021,7 +1264,8 @@ contract Empire is IERC20, Ownable {
     }
 
     /**
-     * @notice  The onlyOwner will withdraw this token to `recipient`.
+     * @notice  Owner will withdraw Empire token and then will use to benefit the holders.
+     *          The onlyOwner will withdraw this token to `recipient`.
      */
     function withdraw(address recipient, uint256 tAmount) external onlyOwner {
         require(recipient != address(0), "ERC20: transfer to the zero address");
@@ -1046,7 +1290,8 @@ contract Empire is IERC20, Ownable {
     }
 
     function setBridge(address _bridge) external onlyOwner {
-        require(bridge != _bridge, "Same Bridge!");
+        require(_bridge != address(0), "Zero Address");
+        require(bridge != _bridge, "Same Bridge");
         bridge = _bridge;
 
         emit LogSetBridge(msg.sender, bridge);
