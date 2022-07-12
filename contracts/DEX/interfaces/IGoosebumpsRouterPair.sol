de3ca96f1cffccf080f45baefcaba7d50b39c909
CryptoSnowPrince <topdirector2017@gmail.com>
2022-07-12 17:33:09 +0200

    remove DEX

--- contracts/DEX/interfaces/IGoosebumpsRouterPair.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity 0.8.7;
-
-import "./IGoosebumpsPair.sol";
-
-interface IGoosebumpsRouterPair is IGoosebumpsPair {
-    function swap(uint amount0Out, uint amount1Out, address to) external;
-}
