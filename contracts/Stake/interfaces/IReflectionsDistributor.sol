eb9ca03ce56bc419a23d6c718a849d8e8ef88fb6
CryptoSnowPrince <topdirector2017@gmail.com>
2022-07-12 16:23:10 +0200

    lock compiler version

--- contracts/Stake/interfaces/IReflectionsDistributor.sol
+++ contracts/Stake/interfaces/IReflectionsDistributor.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.7;
+pragma solidity 0.8.7;
 
 interface IReflectionsDistributor {
     function deposit(address _user, uint256 _amount) external;
