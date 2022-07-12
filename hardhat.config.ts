4106d83ff6c0d6b2da0c959306e34f7729b2eede
CryptoSnowPrince <topdirector2017@gmail.com>
2022-07-12 16:17:27 +0200

    lock compiler version

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -26,51 +26,6 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.5.0",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 2000
-          }
-        }
-      },
-      {
-        version: "0.6.12",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 2000
-          }
-        }
-      },
-      {
-        version: "0.7.3",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 2000
-          }
-        }
-      },
-      {
-        version: "0.8.0",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 2000
-          }
-        }
-      },
-      {
-        version: "0.8.6",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200
-          }
-        }
-      },
-      {
         version: "0.8.7",
         settings: {
           optimizer: {
