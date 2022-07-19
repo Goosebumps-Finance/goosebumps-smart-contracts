844d8a993c24b634d3682b21bb47a256c0f795dc
CryptoSnowPrince <topdirector2017@gmail.com>
2022-07-19 05:17:09 +0200

    update and redeploy stakingwithfixedlocktimeandreflection

--- scripts/Earn/deployStakingWithFixedLockTimeAndReflection.ts
+++ scripts/Earn/deployStakingWithFixedLockTimeAndReflection.ts
@@ -15,9 +15,9 @@ async function main() {
 
   // We get the contract to deploy
 
-  const _stakeToken = "0xD6cf8aeEeBf84C43e28934D6D743820195BBa59C";
-  const _rewardsToken = "0xA1a83bC7712f65E646933a5E00A90537Fc847835";
-  const _treasury = "0x69000D6Da4eedf452a1e2048BEeEc901BcdCE82E";
+  const _stakeToken = "0xE7C6D00B5314EE2651Df6E18e84d6d6dF0EA96a6";
+  const _rewardsToken = "0xf674Ddfd4e2Fb34D36496B77715409127fBDEfEd";
+  const _treasury = "0xC1cb7F497D5CAE9877423AF91a7ab5b8e083AE11";
   const _rewardWallet = "0x69000D6Da4eedf452a1e2048BEeEc901BcdCE82E";
   const _rewardPerBlockTokenN = 100;
   const _rewardPerBlockTokenD = 100;
