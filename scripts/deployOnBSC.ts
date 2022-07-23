896f0c8950a00ac203a47a4aa4f773d0a8d4d85e
CryptoSnowPrince <topdirector2017@gmail.com>
2022-07-23 21:02:55 +0200

    update bsc mainnet deploy script

--- scripts/deployOnBSC.ts
+++ scripts/deployOnBSC.ts
@@ -19,250 +19,15 @@ async function main() {
 
     const deployer = accountList[0];
 
-    // BSC testnet
-
-    // Main MockEmpire token
-    console.log("=====================MockEmpire=====================")
-    const _router = "0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3";
-    const _marketingWallet = accountList[1].address;
-    const _teamWallet = accountList[2].address;
-    const _liquidityWallet = accountList[3].address;
-    const sTokenName_ = "Mock Empire"
-    const sTokenSymbol_ = "MockEMPIRE"
-    console.log("_router: ", _router)
-    console.log("_marketingWallet: ", _marketingWallet)
-    console.log("_teamWallet: ", _teamWallet)
-    console.log("_liquidityWallet: ", _liquidityWallet)
-    console.log("sTokenName_: ", sTokenName_)
-    console.log("sTokenSymbol_: ", sTokenSymbol_)
-    const Empire = await ethers.getContractFactory("MockEmpire");
-    const empire = await Empire.deploy(_router, _marketingWallet, _teamWallet, _liquidityWallet, sTokenName_, sTokenSymbol_);
-    await empire.deployed();
-    console.log("MockEmpire deployed to:", empire.address);
-    console.log("delay...")
-    sleep(1000)
-    console.log("==========================================")
-
-    // Approve MockEmpire to router
-    console.log("=====================MockEmpire Approve=====================")
-    const txApprove = await empire.approve(_router, ethers.utils.parseUnits("100000000", 9));
-    const retTxApprove = await txApprove.wait();
-    console.log("MockEmpire Approve txHash: ", retTxApprove.transactionHash);
-    console.log("delay...")
-    sleep(1000)
-    console.log("==========================================")
-
-    // Mock BUSD token
-    console.log("=====================Mock BUSD=====================")
-    const BusdName_ = "MockBUSD"
-    const BusdSymbol_ = "MockBUSD"
-    console.log("BusdName_: ", BusdName_)
-    console.log("BusdSymbol_: ", BusdSymbol_)
-    const MockToken = await ethers.getContractFactory("MockToken");
-    const busdToken = await MockToken.deploy(BusdName_, BusdSymbol_);
-    await busdToken.deployed();
-    console.log("busdToken deployed to:", busdToken.address);
-    console.log("delay...")
-    sleep(1000)
-    console.log("==========================================")
-
-    // Appove MockBUSD to router
-    console.log("=====================Mock BUSD Appove=====================")
-    const txMockBUSDApprove = await busdToken.approve(_router, ethers.utils.parseUnits("100000000", 18));
-    const retTxMockBUSDApprove = await txMockBUSDApprove.wait();
-    console.log("MockBUSD Approve txHash: ", retTxMockBUSDApprove.transactionHash);
-    console.log("delay...")
-    sleep(1000)
-    console.log("==========================================")
-
-    // AddLiquidityETH (MockEmpire/BNB)
-    console.log("=====================AddLiquidityETH (MockEmpire/BNB)=====================")
-    const _routerABI =
-        [
-            {
-                "inputs": [
-                    {
-                        "internalType": "address",
-                        "name": "tokenA",
-                        "type": "address"
-                    },
-                    {
-                        "internalType": "address",
-                        "name": "tokenB",
-                        "type": "address"
-                    },
-                    {
-                        "internalType": "uint256",
-                        "name": "amountADesired",
-                        "type": "uint256"
-                    },
-                    {
-                        "internalType": "uint256",
-                        "name": "amountBDesired",
-                        "type": "uint256"
-                    },
-                    {
-                        "internalType": "uint256",
-                        "name": "amountAMin",
-                        "type": "uint256"
-                    },
-                    {
-                        "internalType": "uint256",
-                        "name": "amountBMin",
-                        "type": "uint256"
-                    },
-                    {
-                        "internalType": "address",
-                        "name": "to",
-                        "type": "address"
-                    },
-                    {
-                        "internalType": "uint256",
-                        "name": "deadline",
-                        "type": "uint256"
-                    }
-                ],
-                "name": "addLiquidity",
-                "outputs": [
-                    {
-                        "internalType": "uint256",
-                        "name": "amountA",
-                        "type": "uint256"
-                    },
-                    {
-                        "internalType": "uint256",
-                        "name": "amountB",
-                        "type": "uint256"
-                    },
-                    {
-                        "internalType": "uint256",
-                        "name": "liquidity",
-                        "type": "uint256"
-                    }
-                ],
-                "stateMutability": "nonpayable",
-                "type": "function"
-            },
-            {
-                "inputs": [
-                    {
-                        "internalType": "address",
-                        "name": "token",
-                        "type": "address"
-                    },
-                    {
-                        "internalType": "uint256",
-                        "name": "amountTokenDesired",
-                        "type": "uint256"
-                    },
-                    {
-                        "internalType": "uint256",
-                        "name": "amountTokenMin",
-                        "type": "uint256"
-                    },
-                    {
-                        "internalType": "uint256",
-                        "name": "amountETHMin",
-                        "type": "uint256"
-                    },
-                    {
-                        "internalType": "address",
-                        "name": "to",
-                        "type": "address"
-                    },
-                    {
-                        "internalType": "uint256",
-                        "name": "deadline",
-                        "type": "uint256"
-                    }
-                ],
-                "name": "addLiquidityETH",
-                "outputs": [
-                    {
-                        "internalType": "uint256",
-                        "name": "amountToken",
-                        "type": "uint256"
-                    },
-                    {
-                        "internalType": "uint256",
-                        "name": "amountETH",
-                        "type": "uint256"
-                    },
-                    {
-                        "internalType": "uint256",
-                        "name": "liquidity",
-                        "type": "uint256"
-                    }
-                ],
-                "stateMutability": "payable",
-                "type": "function"
-            },
-        ]
-
-    const router = new ethers.Contract(_router, _routerABI, deployer);
-
-    const provider = new ethers.providers.JsonRpcProvider("https://data-seed-prebsc-2-s3.binance.org:8545/")
-    const timeStamp = (await provider.getBlock("pending")).timestamp
-    console.log("timeStamp: ", timeStamp)
-    const txAddLiquidityETH = await router.addLiquidityETH(
-        empire.address,
-        ethers.utils.parseUnits("1000000", 9), 0, 0,
-        deployer.address,
-        timeStamp + 1000,
-        { value: ethers.utils.parseEther("0.05") }
-    );
-    const retTxAddLiquidityETH = await txAddLiquidityETH.wait();
-    console.log("AddLiquidityETH txHash: ", retTxAddLiquidityETH.transactionHash)
-    console.log("delay...")
-    sleep(1000)
-    console.log("==========================================")
-
-    // AddLiquidity (MockEmpire/MockBUSD)
-    console.log("=====================AddLiquidity (MockEmpire/MockBUSD)=====================")
-    const txAddLiquidity = await router.addLiquidity(
-        empire.address,
-        busdToken.address,
-        ethers.utils.parseUnits("1000000", 9),
-        ethers.utils.parseUnits("5000", 18), 0, 0,
-        deployer.address,
-        timeStamp + 1000,
-    );
-    const retTxAddLiquidity = await txAddLiquidity.wait();
-    console.log("AddLiquidity txHash: ", retTxAddLiquidity.transactionHash)
-    console.log("delay...")
-    sleep(1000)
-    console.log("==========================================")
-
-    // Staking Reward token
-    console.log("=====================Staking Reward token=====================")
-    const sRewardName_ = "StakingReward"
-    const sRewardSymbol_ = "sReward"
-    console.log("sRewardName_: ", sRewardName_)
-    console.log("sRewardSymbol_: ", sRewardSymbol_)
-    const sRewardToken = await MockToken.deploy(sRewardName_, sRewardSymbol_);
-    await sRewardToken.deployed();
-    console.log("sRewardToken deployed to:", sRewardToken.address);
-    console.log("delay...")
-    sleep(1000)
-    console.log("==========================================")
-
-    // Farming Reward token
-    console.log("=====================Farming Reward token=====================")
-    const fRewardName_ = "FarmingReward"
-    const fRewardSymbol_ = "fReward"
-    console.log("fRewardName_: ", fRewardName_)
-    console.log("fRewardSymbol_: ", fRewardSymbol_)
-    const fReward = await MockToken.deploy(fRewardName_, fRewardSymbol_);
-    await fReward.deployed();
-    console.log("fReward deployed to:", fReward.address);
-    console.log("delay...")
-    sleep(1000)
-    console.log("==========================================")
-
+    // BSC mainnet
+    const empireAddress = "0x293C3Ee9ABaCb08BB8ceD107987F00EfD1539288" // Empire V1
+    const lpBNB = "0x60c9bd6EEE0a911b987A749F0DB0F24D79bCDcf5" // EmpireV1/BNB
+    const lpBUSD = "0x591dEFEccE63464759b3B606F4ec2974F0738462" // EmpireV1/BUSD
+    
     // Staking
     console.log("=====================Staking=====================")
-    const _stakeToken = empire.address;
-    const _rewardsToken = sRewardToken.address;;
+    const _stakeToken = empireAddress;
+    const _rewardsToken = empireAddress;
     const _treasury = deployer.address;
     const _rewardWallet = deployer.address;
     const _rewardPerBlockTokenN = 100;
@@ -387,40 +152,8 @@ async function main() {
 
     // Farming
     console.log("=====================Farming=====================")
-    const WETH = "0xae13d989dac2f0debff460ac112a837c89baa7cd"
-    const _factory = "0xb7926c0430afb07aa7defde6da862ae0bde767bc"
-    const _factoryABI =
-        [
-            {
-                "constant": true,
-                "inputs": [
-                    {
-                        "internalType": "address",
-                        "name": "",
-                        "type": "address"
-                    },
-                    {
-                        "internalType": "address",
-                        "name": "",
-                        "type": "address"
-                    }
-                ],
-                "name": "getPair",
-                "outputs": [
-                    {
-                        "internalType": "address",
-                        "name": "",
-                        "type": "address"
-                    }
-                ],
-                "payable": false,
-                "stateMutability": "view",
-                "type": "function"
-            },
-        ]
-    const factory = new ethers.Contract(_factory, _factoryABI, deployer);
-    const _lpBNB = await factory.getPair(empire.address, WETH)
-    const _frewardsToken = fReward.address;
+    const _lpBNB = lpBNB
+    const _frewardsToken = empireAddress;
 
     const GoosebumpsFarming = await ethers.getContractFactory("GoosebumpsFarming");
     const goosebumpsFarming = await GoosebumpsFarming.deploy(_lpBNB, _frewardsToken, _treasury, _rewardWallet, _rewardPerBlockTokenN, _rewardPerBlockTokenD);
@@ -438,7 +171,7 @@ async function main() {
 
     // Farming(Empire/BUSD)
     console.log("=====================Farming(Empire/BUSD)=====================")
-    const _lpBUSD = await factory.getPair(empire.address, busdToken.address)
+    const _lpBUSD = lpBUSD
     const goosebumpsFarmingBUSD = await GoosebumpsFarming.deploy(_lpBUSD, _frewardsToken, _treasury, _rewardWallet, _rewardPerBlockTokenN, _rewardPerBlockTokenD);
     await goosebumpsFarmingBUSD.deployed();
     console.log("_lpToken: ", _lpBUSD)
